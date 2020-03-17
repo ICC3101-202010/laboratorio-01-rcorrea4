7ecb90b0ae41eac953e6a81e9c84eac79db2901e
rcorrea4 <rcorrea4@miuandes.cl>
2020-03-17 17:29:43 -0300

    second commit

--- Lab1Rcorrea4/Lab1Rcorrea4/Player.cs
+++ Lab1Rcorrea4/Lab1Rcorrea4/Player.cs
@@ -8,5 +8,22 @@ namespace Lab1Rcorrea4
 {
     class Player
     {
+        private string name;
+        private string lastName;
+
+        public Player(string name, string lastName)
+        {
+            this.name = name;
+            this.lastName = lastName;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+
+        public int lanzar(int min=0, int max=2)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
     }
 }
--- Lab1Rcorrea4/Lab1Rcorrea4/Program.cs
+++ Lab1Rcorrea4/Lab1Rcorrea4/Program.cs
@@ -10,6 +10,14 @@ namespace Lab1Rcorrea4
     {
         static void Main(string[] args)
         {
+            Player player1 = new Player("Bob", "Kunga");
+            Player player2 = new Player("Juan", "Perez");
+            int lanzado = player1.lanzar();
+            
+            
+            Console.WriteLine( player1.Name +" " +player1.LastName + "lanzo " + lanzado);
+            Console.ReadLine();
+
         }
     }
 }
